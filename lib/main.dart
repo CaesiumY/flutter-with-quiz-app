f9e0bd54d884de420075955ae39d6c27d46b49be
CaesiumY <mn0658@naver.com>
2020-05-22 13:29:24 +0900

    feat: Init project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quiz/screen/screen_home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Flutter Quiz APP', home: HomeScreen());
+  }
+}
