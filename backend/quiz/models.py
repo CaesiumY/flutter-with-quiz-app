5617f80bff2f9a16c5d149c623cbe094b322f3ee
CaesiumY <mn0658@naver.com>
2020-05-30 13:25:12 +0900

    feat: Add QuizModel

--- backend/quiz/models.py
+++ backend/quiz/models.py
@@ -1,4 +1,15 @@
 from django.db import models
 
 # Create your models here.
-    
+
+
+class QuizModel(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    answer = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("QuizModel_detail", kwargs={"pk": self.pk})
