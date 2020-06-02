5eb135af14666034dded5efcae688ed5de8dbd22
CaesiumY <mn0658@naver.com>
2020-06-02 11:44:03 +0900

    feat: Add randomQuiz view

--- backend/quiz/views.py
+++ backend/quiz/views.py
@@ -1,8 +1,17 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+import random
 from .models import QuizModel
 from .serializer import QuizSerializer
 
 @api_view(['GET'])
 def helloAPI(request):
     return Response('hello world')
+
+@api_view(['GET'])
+def randomQuiz(request, id):
+    totalQuizs = QuizModel.objects.all()
+    randomQuizs = random.sample(list(totalQuizs), id)
+    serializer = QuizSerializer(randomQuizs, many=True)
+
+    return Response(serializer.data)
